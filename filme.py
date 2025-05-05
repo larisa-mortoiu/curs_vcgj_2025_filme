0fc330f2dd686b2340c237fc1862380465bb26dd
Larisa <larisamortoiu@gmail.com>
2025-05-05 23:50:33 +0000

    Modified web app pages

--- filme.py
+++ filme.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, url_for
-
+from livereload import Server
+import app.lib.dynasty_description as description_dy
+import app.lib.dynasty_cast as cast_dy
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["TESTING"] = True
+
 @app.route('/')
+def index():
+    return render_template('homepage.html')
 
-
+@app.route('/dynasty', methods=['GET'])
 def dynasty():
-    return "<h1>Dynasty</h1>"
+    return render_template('dynasty.html')
+
+@app.route('/dynasty/description', methods=['GET'])
+def dynasty_description():
+    description = description_dy.get_description()
+    return render_template('dynasty_description.html', description = description)
+
+@app.route('/dynasty/cast', methods=['GET'])
+def dynasty_cast():
+    cast = cast_dy.get_cast()
+    return render_template('dynasty_cast.html', cast = cast)
+
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    server = Server(app.wsgi_app)
+    server.serve(port=5011, debug=True)
