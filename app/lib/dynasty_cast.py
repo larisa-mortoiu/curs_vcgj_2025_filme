5d786e79a62bf9ab7f5d9629a857a7d0b09ebf27
Larisa <larisamortoiu@gmail.com>
2025-05-07 19:35:41 +0000

    Updated gitignore

---
+++ app/lib/dynasty_cast.py
@@ -0,0 +1,13 @@
+def get_cast():
+    cast = """<p>Elizabeth Gillies – Fallon Carrington - este o actriță și cântăreață americană, cunoscută pentru rolul ei în Victorious (Nickelodeon). În Dynasty, o interpretează pe Fallon Carrington, o femeie tânără, ambițioasă și manipulatoare, care luptă pentru controlul imperiului familiei sale.</p>
+
+<p>Grant Show – Blake Carrington - este un actor american, cunoscut din diverse producții de televiziune, inclusiv din Melrose Place și Private Practice. În Dynasty, el joacă rolul lui Blake Carrington, tatăl lui Fallon și cel care încearcă să mențină puterea familiei Carrington.</p>
+
+<p>Daniella Alonso – Cristal Carrington - este o actriță americană de origine portoricană și japoneză, cunoscută din seriale precum The Night Shift și The Resident. În Dynasty, ea joacă rolul Cristal Carrington, noua soție a lui Blake, care intră în conflict cu familia Carrington.</p>
+
+<p>Rafael de la Fuente – Sam Jones - este un actor și model venezuelean. El joacă rolul lui Sam Jones, un personaj care ajunge să devină o piesă importantă în viața lui Fallon și în dinamica familiei Carrington. Rafael este cunoscut și pentru aparițiile sale în serialul Empire.</p>
+
+<p>Sam Adegoke – Jeff Colby - este un actor nigerian-american, cunoscut pentru rolul său din Dynasty și pentru aparițiile din serialele Shameless și The Resident. În Dynasty, Sam joacă rolul lui Jeff Colby, un personaj complex care devine un adversar important al familiei Carrington.</p>
+
+<p>Alan Dale – Joseph Anders - este un actor australian, care a apărut în numeroase seriale de televiziune, printre care The OC, Lost și The West Wing. În Dynasty, el joacă rolul lui Joseph Anders, majordomul familiei Carrington, care are un rol esențial în menținerea ordinii în casa Carrington.</p>"""
+    return cast
---
+++ app/lib/dynasty_description.py
@@ -0,0 +1,8 @@
+def get_description():
+    
+    description = """
+    Dynasty is an American prime time television soap opera that aired on ABC from January 12, 1981, to May 11, 1989. 
+    The series was created by Richard and Esther Shapiro and was produced by Aaron Spelling and Douglas S. Cramer. 
+    It is a reboot of the 1976 series of the same name and follows the lives of the wealthy Carrington family in Denver, Colorado.
+    """
+    return description
