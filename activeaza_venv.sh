23e9dcac19dd55a23b79276e02172692f7de598e
alexandradina40 <125584777+alexandradina40@users.noreply.github.com>
2025-05-13 00:57:17 +0300

    Update activeaza_venv.sh

--- activeaza_venv.sh
+++ activeaza_venv.sh
@@ -5,15 +5,15 @@
 
 # Dacă nu există, îl creează și instalează requirements
 if [ $? -eq 0 ]; then
-    echo "✅ SUCCESS: venv was activated."
+    echo "SUCCESS: venv was activated."
 else
-    echo "⚠️  FAIL: .venv not found. Creating one..."
+    echo "FAIL: .venv not found. Creating one..."
     python3 -m venv .venv
     source .venv/bin/activate
 
-    echo "🔁 Installing requirements..."
+    echo "Installing requirements..."
     pip install --upgrade pip
     pip install -r requirements.txt
 
-    echo "✅ venv created and requirements installed."
+    echo "venv created and requirements installed."
 fi
