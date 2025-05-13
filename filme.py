5b3e37db93700ce7c05e6d5c4d087aec423856ac
SARMAL11 <adil.mologani@gmail.com>
2025-05-13 21:35:27 +0000

    am mai adaugat continut in pagini

--- filme.py
+++ filme.py
@@ -9,6 +9,15 @@ app.config["TESTING"] = True
 def index():
     return render_template('home_cars.html')
 
+@app.route('/descriere')
+def descriere():
+    return render_template('descriere_cars.html')
+
+@app.route('/cars2006')
+def cars_2006():
+    return render_template('cars_2006.html')
+
+
 if __name__ == "__main__":
     server = Server(app.wsgi_app)
     server.serve(debug=True)
