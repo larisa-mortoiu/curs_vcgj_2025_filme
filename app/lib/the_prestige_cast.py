ad43a1dcb7164c9c13b85a387c35c09fbf1ede35
Dina <alexandra.dina40@yahoo.com>
2025-05-14 20:22:36 +0000

    Completed Jenkinsfile

--- app/lib/the_prestige_cast.py
+++ app/lib/the_prestige_cast.py
@@ -2,6 +2,7 @@ def get_actori():
     return [
         "Christian Bale",
         "Hugh Jackman",
+        "David Bowie",
         "Piper Perabo",
         "Rebecca Hall",
         "Scarlett Johansson",
