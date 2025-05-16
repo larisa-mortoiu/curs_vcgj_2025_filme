4545d7e33e861cb8949047474cc90caf2f0f1cd3
Ichim-Ionut <ichim_ionut10@yahoo.com>
2025-05-16 19:48:38 +0300

    Scrierea codului principal al aplicatiei

---
+++ filme.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/descriere')
+def descriere():
+    return render_template('descriere.html')
+
+@app.route('/actori')
+def actori():
+    return render_template('actori.html')
+
+@app.route('/trailer')
+def trailer():
+    return render_template('trailer.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
