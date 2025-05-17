c72ddd1a1dfede27d9767cd5af79fba46107ffc6
zGunterr <phamhoangimp@gmail.com>
2025-05-17 15:46:47 +0000

    modified cast and description

--- filme.py
+++ filme.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, send_from_directory
+from app.lib.description import get_description
+from app.lib.cast import get_cast
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -8,11 +10,13 @@ def home():
 
 @app.route('/description')
 def description():
-    return render_template('description.html')
+    movie = get_description()
+    return render_template('description.html', movie=movie)
 
 @app.route('/cast')
 def cast():
-    return render_template('cast.html')
+    cast_list = get_cast()
+    return render_template("cast.html", cast=cast_list)
 
 # Optional: route to serve static files (images, styles)
 @app.route('/static/<path:filename>')
