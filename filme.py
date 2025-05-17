623b1564b36c7e82419d73a0e803ef6767366327
Alex <alex@Ubuntu22.04.5.myguest.virtualbox.org>
2025-05-17 21:01:10 +0300

    Am creat Dockerfile si Jenkinsfile

---
+++ filme.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/actors')
+def actors():
+    return render_template('actors.html')
+
+@app.route('/description')
+def description():
+    return render_template('description.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port = 5000)
