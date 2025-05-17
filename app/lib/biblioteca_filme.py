8492aa292dae3375996cfe8abde1be4643aaa755
SirCheeseKnight <theknightofthecheese@gmail.com>
2025-05-17 20:55:37 +0300

    actualizat distributia cu poze

--- app/lib/biblioteca_filme.py
+++ app/lib/biblioteca_filme.py
@@ -3,49 +3,49 @@ def distribuite_hotfuzz():
         {
             "personaj": "Nicholas Angel"
             "actor": "Simon Pegg"
-            "fotografie": 
+            "fotografie": "simon_pegg.jpg"
             "descriere": "Ofițer de poliție foarte competent din Londra care este transferat forțat la un post din provincie. Rata crimei este aparent zero, dar rata de accidente este extrem de mare. Datorită zelului său, Nicholas descoperă că lucrurile nu sunt deloc așa cum par."
         },
         {
             "personaj": "Danny Butterman"
             "actor": "Nick Frost"
-            "fotografie": 
+            "fotografie": "nick_frost.jpg"
             "descriere": "Partenerul lui Nicholas Angel în Sandford. Este un polițer local bine intenționat, dar naiv și dornic de acțiune, a cărui viziune despre munca de poliție este puternic influențată de filmele de acțiune."
         },
         {
             "personaj": "Frank Butterman"
             "actor": "Jim Broadbent"
-            "fotografie": 
+            "fotografie": "jim_broadbent.jpg"
             "descriere": "Tatăl lui Danny și inspectorul șef al secției de poliție din Sandford. Pare un om blând și jovial, dar ascunde secrete legate de adevărata natură a evenimentelor din sat."
         },
         {
             "personaj": "Simon Skinner"
             "actor": "Timothy Dalton"
-            "fotografie": 
+            "fotografie": "timothy_dalton.jpg"
             "descriere": "Managerul supermarketului local și o figură proeminentă în comunitatea din Sandford. Este un personaj carismatic, dar cu o latură sinistră."
         },
         {
             "personaj": "Doris Thatcher"
             "actor": "Olivia Colman"
-            "fotografie": 
+            "fotografie": "olivia_colman.jpg"
             "descriere": "Singura femeie polițist din secția Sandford. Este o persoană jovială și relaxată, cu un simț al umorului direct."
         },
         {
             "personaj": "Inspectorul șef Kenneth"
             "actor": "Bill Nighy"
-            "fotografie": 
+            "fotografie": "bill_nighy.jpg"
             "descriere": "Superiorul lui Nicholas Angel la poliția metropolitană din Londra, cel care decide transferul său."
         },
         {
             "personaj": "Andy Wainwright"
             "actor": "Paddy Considine"
-            "fotografie": 
+            "fotografie": "paddy_considine.jpg"
             "descriere": "Unul dintre detectivii locali din Sandford. Este leneș, cinic și nu ia prea în serios munca de poliție, preferând să petreacă timpul la pub"
         },
         {
             "personaj": "Andy Cartwright"
             "actor": "Rafe Spall"
-            "fotografie": 
+            "fotografie": "rafe_spall.jpg"
             "descriere": "Celălalt detectiv din Sandford, partenerul lui Wainwright. Similar cu acesta, este lipsit de inițiativă și pare mai interesat de relaxare decât de rezolvarea cazurilor."
         },
         
